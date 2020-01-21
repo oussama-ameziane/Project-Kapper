6e80e8b7da4c3ae9a2dfc9180293f17b63c81bb7
Oussama <oussama.ameziane@student.hu.nl>
2020-01-21 14:51:34 +0100

    code clean

--- Website kapper/static/main.js
+++ Website kapper/static/main.js
@@ -7,7 +7,8 @@ $(function () {
     $("#schemaTable tbody tr").on('click','.reservering', function()
     {
         let thisElement = $(this);
-        if (thisElement.hasClass('bg-success')) {
+        if (thisElement.hasClass('bg-success')) 
+        {
             document.getElementById('id01').style.display='block';
             var ghettoTimeString = thisElement.attr("id");
             $("#tijdstipForm").val(`${ghettoTimeString.slice(0, 2)}:${ghettoTimeString.slice(2)}`);
@@ -15,7 +16,8 @@ $(function () {
         }
     })
 
-    $("#id01form").submit(function (obj) {
+    $("#id01form").submit(function (obj) 
+    {
         var tijdValue = $("#tijdstipForm").val();
         var kapperValue = $("#keuze-kapper").val();
         var datumValue = $("#TheDate").val();
@@ -25,46 +27,14 @@ $(function () {
         .attr("name", "datum")
         .attr("value",$("#TheDate").val()).appendTo(this);
         window.localStorage.setItem(kokerGeileString, "yes");
+        alert("Uw reservering is geslaagd");
     });
 });
 
-// $(function () {
-//     $("#id01").on('click','.resbtn', function()
-//     {
-//         document.getElementById('id01').style.display='none';
-//         let clickid = document.getElementById('id01').dataset.clickedelementid.toString();
-//         alert(clickid);
-//         let thisElement = document.getElementById(clickid);
-//         alert(thisElement.id);
-//         if (thisElement.hasClass('bg-success')) {  
-//             thisElement.addClass('bg-danger');
-//             thisElement.removeClass('bg-success');
-//             thisElement.children('h2').text('GERESERVEERD');
-//         }
-//     })
-// });
-
-//$(function () {
-    //$("#schemaTable tbody tr").on('click', '.reservering', function()
-     //{
-        //let thisElement = $(this);
-        //if (thisElement.hasClass('bg-danger')) {
-            //thisElement.removeClass('bg-danger');
-            //thisElement.addClass('bg-success');
-            //thisElement.children('h2').text('BESCHIKBAAR');
-        //}
-        //else {
-            //document.getElementById('id01').style.display='block'
-            //thisElement.addClass('bg-danger');
-            //thisElement.removeClass('bg-success');
-            //thisElement.children('h2').text('GERESERVEERD');
-        //}
-    //})
-//});
-
 $(document).ready(function() {
 
-    $("#TheDate,#keuze-kapper").change(function() {
+    $("#TheDate,#keuze-kapper").change(function() 
+    {
         $(".reservering").each(function(i, obj) {
             var ghettoTimeString = $(this).attr("id");
             var realTime = `${ghettoTimeString.slice(0, 2)}:${ghettoTimeString.slice(2)}`;
@@ -84,19 +54,10 @@ $(document).ready(function() {
             }
           });
     });
-  /*  var date = new Date();
+  
 
-    var day = date.getDate();
-    var month = date.getMonth() + 1;
-    var year = date.getFullYear();
-
-    if (month < 10) month = "0" + month;
-    if (day < 10) day = "0" + day;
-
-    var today = year + "-" + month + "-" + day;     */  
-  //  $("#theDate").attr("value", today);
-
-  $(".reservering").each(function(i, obj) {
+  $(".reservering").each(function(i, obj) 
+  {
     var ghettoTimeString = $(this).attr("id");
     var realTime = `${ghettoTimeString.slice(0, 2)}:${ghettoTimeString.slice(2)}`;
     var kapperValue = $("#keuze-kapper").val();
